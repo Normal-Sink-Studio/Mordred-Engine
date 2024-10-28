eb64bbc174cfa5e4414071b2363a5921418b7cee
Oreboat <tharper04@gmail.com>
2024-10-27 19:54:36 -0500

    I don't even know anymore

--- build.zig
+++ build.zig
@@ -18,6 +18,16 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("gdi32");
     exe.linkSystemLibrary("shell32");
 
+    const GLFW_Path = b.path("dependencies/windows/64 bit/GLFW/lib-vc2022/");
+    const GLFW_Headers = b.path("dependencies/windows/64 bit/GLFW/include/");
+    // Add the GLFW headers to the include path
+    exe.addIncludePath(GLFW_Headers);
+    // Link the GLFW library by adding it to the library path
+    exe.addLibraryPath(GLFW_Path);
+
+    exe.linkSystemLibrary("glfw3");
+    // Mark the executable as an installable artifact
+
     // Define the path to the GLEW library and its headers
     const GLEW_Path = b.path("dependencies/windows/64 bit/GLEW/glew-2.1.0/lib/");
     const GLEW_Headers = b.path("dependecies/windows/64 bit/GLEW/glew-2.1.0/include");
@@ -28,4 +38,6 @@ pub fn build(b: *std.Build) void {
     exe.addLibraryPath(GLEW_Path);
 
     exe.linkSystemLibrary("glew32s");
+
+    b.installArtifact(exe);
 }
